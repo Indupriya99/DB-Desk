4ab5bb41ce4d0617282286f05c36d77129c8d750
Aswin Sampath <aswinsampath1401@gmail.com>
2021-08-14 12:03:22 +0530

    Resolved all bugs

--- src/Pages/Admin/Login/AdminLogin.js
+++ src/Pages/Admin/Login/AdminLogin.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './adminLogin.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,14 @@ export default class AdminLogin extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    // or simply just AOS.init();
+    AOS.init({
+      // initialise with other settings
+      duration : 2000
+    });
   }
 
   handleChange(event) {
@@ -70,34 +79,23 @@ export default class AdminLogin extends Component {
     return (
       <div className='adjust'>
         <div className="auth-wrapper">
-        <div className="auth-inner">
+        <div data-aos={'zoom-out-up'} className="auth-inner">
         <form onSubmit={this.handleSubmit}>
-                <h3>Admin Sign In</h3>
+                <h3  className='admin-sign-in'>Admin Sign In</h3>
                 <div class="container">
-            <label for="email">Email Address:</label>
-            <input 
-              type="text" 
-              name="email" 
-              value={this.state.input.email}
-              onChange={this.handleChange}
-              class="form-control" 
-              placeholder="Enter email" 
-              id="email" />
-   
-              <div className="text-danger">{this.state.errors.email}</div>
-          </div>
-                  
-
-                   
-                    &nbsp;&nbsp;&nbsp;
-
-            
-                <div class="wrapper">
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-
+                  <label for="email" className='emailLabel'>Email Address:</label>
+                  <input 
+                      type="text" 
+                      name="email" 
+                      value={this.state.input.email}
+                      onChange={this.handleChange}
+                      // class="form-control" 
+                      className='myInput'
+                      placeholder="Enter email" 
+                      id="email" />
+                  <div className="text-danger anyErros">{this.state.errors.email}</div>
                 </div>
-               
-               
+                <button type="submit" className="mybtn">Sign In</button>
             </form>
         </div>
         </div>
